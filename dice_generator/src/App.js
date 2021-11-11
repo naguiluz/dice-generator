1b7a104e486e10d3d55d3b7be2d63597050a5238
naguiluz <nicolasaguiluz2@gmail.com>
2021-11-10 22:39:23 -0600

    Add number of dice functionality
    added loops to add dice based on for amount and total result
    
    updated state with rolledPrint to split array of numbers rolled for user display

--- dice_generator/src/App.js
+++ dice_generator/src/App.js
@@ -1,29 +1,62 @@
 import React, { Component } from 'react'
 import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
 import './App.scss'
 
 class App extends Component {
 	constructor(props) {
 		super(props)
-    this.state = {
-      rollValue: 0
-    }
-    // this.rollDice = this.rollDice.bind(this)
+		this.state = {
+			result: 0,
+      amount: 1,
+      numsRolled: '',
+      rolledPrint: ''
+		}
+		// this.rollDice = this.rollDice.bind(this)
 	}
+
+  handleChange = (event) =>
+    this.setState({
+      [event.target.name]:event.target.value
+    })
+
 	rollDice = (sides) => {
-			let result = Math.ceil(Math.random() * sides)
-			console.log(`You rolled a ${result}`)
-      this.setState({
-        rollValue: result
-      })
-      
+		let numsRolled = []
+    let result = 0
+    for (let i = 0; i < this.state.amount; i++) {
+			numsRolled.push(Math.ceil(Math.random() * sides))
+    }
+    for (let i = 0; i < numsRolled.length; i++) {
+      result += numsRolled[i]
+    }
+		console.log(`You rolled ${numsRolled.join()}`)
+    console.log(`Giving a total of ${result}`)
+    let rolledPrint = numsRolled.join(' + ')
+   
+  
+		this.setState({
+			result: result,
+      numsRolled: numsRolled,
+      rolledPrint: rolledPrint
+		})
 	}
 
 	render() {
-    // let result = this.rollDice()
+		// let result = this.rollDice()
 		return (
 			<>
 				<div className='App'>
+					<Form>
+              <Form.Group controlId='amount'></Form.Group>
+              <Form.Label>How many dice do you want to roll?</Form.Label>
+              <Form.Control
+                name='amount'
+                value={this.state.amount}
+                placeholder='#'
+                onChange={this.handleChange}
+              />
+              <Form.Group />
+					</Form>
 					<Button variant='warning' onClick={(sides) => this.rollDice(2)}>
 						D2
 					</Button>
@@ -48,11 +81,13 @@ class App extends Component {
 					<Button variant='warning' onClick={(sides) => this.rollDice(100)}>
 						D100
 					</Button>
-					<div id='result'>{this.state.rollValue}</div>
+          <div id='rolled dice'>You rolled: {this.state.rolledPrint}</div>
+					<div id='result'>Totalling: {this.state.result}</div>
 				</div>
 			</>
 		)
 	}
+	
 }
 
 export default App;
